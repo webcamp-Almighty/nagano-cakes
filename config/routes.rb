942782c33bb049091126e7e1321d9f039b14701d
vagrant <vagrant@localhost.localdomain>
2020-06-02 02:58:50 +0000

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :genres, only: [:index, :create, :edit, :update]
-
   devise_for :end_users
 
   devise_for :admins, skip: :all
@@ -11,38 +9,23 @@ Rails.application.routes.draw do
     delete 'admins/sign_out' => 'admins/sessions#destroy', as: 'destroy_admin_session'
   end
 
-
   namespace :admins do
     get 'top' => 'homes#top'
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :end_users, only: [:index, :show, :edit, :update]
     resources :orders, only: [:index, :show, :update]
     resources :genres, only: [:create, :index, :edit, :update]
-    resources :orders_item, only: [:update]
+    resources :order_items, only: [:update]
   end
 
-
-
-
-
-  get 'orders/index'
-  get 'orders/new'
-  get 'orders/show'
-  get 'orders/detail'
-  get 'orders/finish'
-  get 'cart_items/index'
-  get 'deliveries/index'
-  get 'deliveries/edit'
-  get 'end_users/show'
-  get 'end_users/edit'
-  get 'homes/top'
   root 'homes#top'
 
   resources :items, only: [:index, :show]
+
   resources :cart_items, only: [:create, :index, :update, :destroy]
   delete '/cart_items' => 'cart_items#empty'
 
-  resources :orders, only: [:new, :create]
+  resources :orders, only: [:index, :new, :create, :show]
   get '/orders/detail' => 'orders#detail'
   get '/orders/finish' => 'orders#finish'
 
@@ -50,9 +33,6 @@ Rails.application.routes.draw do
   get '/end_users/confirm' => 'end_users#confirm'
   patch '/end_users' => 'end_users#hide'
 
-  resources :deliveries, only: [:index, :create, :edit, :update, :destroy]
-
-  resource :orders, only: [:index, :show]
-
+  resources :deliveries, except: [:new, :show]
 
 end
