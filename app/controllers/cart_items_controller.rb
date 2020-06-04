a414651f0d0f6bf712ea62a4295995f84e5efd5b
vagrant <vagrant@localhost.localdomain>
2020-06-04 05:01:24 +0000

    ショッピングカート画面の作成

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,4 +1,28 @@
 class CartItemsController < ApplicationController
   def index
+  	@cart_items = current_end_user.cart_items
+  end
+
+  def create
+  	@cart_item = current_end_user.cart_items.new(cart_item_params)
+  	@cart_item.end_user_id = current_end_user.id
+   	@cart_item.save
+   	redirect_to cart_items_path
+  end
+
+  def destroy
+  	cart_item = Cartitem.find(params[:id])
+  	cart_item.destroy
+  end
+
+  def empty
+  	end_user = end_user.find(current_end_user.id)
+  	end_user.cart_items.destroy
+  	redirect_to cart_items_path
+  end
+
+  private
+  def cart_item_params
+  	params.require(:cart_item).permit(:number)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  has_many :cart_items
   belongs_to :genre
   attachment :image #refile用
 end
