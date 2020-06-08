8fefac829d8d0e408d95195a13190b0e845fec58
makiharu <kitakazetoshirokuma@gmail.com>
2020-06-08 02:59:22 +0000

    注文情報入力ページ機能追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,12 +9,14 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
-    @order.end_user_id = current_end_user.id
+
     if @order.save
-      redirect_to orders_finish_path
+      redirect_to finish_orders_path
     else
-      render 'index'
+      @cart_items = CartItem.where(end_user_id: current_end_user.id)
+      render :detail, notice: "もう一度やり直してください"
     end
+
   end
 
   def show
@@ -22,8 +24,22 @@ class OrdersController < ApplicationController
   end
 
   def detail
+    @shipping_fees = 800
+    @total_price = @shipping_fees + current_end_user.cart_item_sum
     @cart_items = CartItem.where(end_user_id: current_end_user.id)
     @order = Order.new(order_params)
+
+    # if params[:order][:radio_button] == 1
+    #   @oneorder.postal_code = current_end_user.postal_code
+    #   @oneorder.address = current_user.address
+    # else params[:order][:radio_button] == 2
+    #   @oneorder.postal_code =
+    # if @order.save
+    #   redirect_to finish_orders_path
+    # else
+    #   flash[:notice] = "もう一度やり直してください"
+    #   render :detail
+    # end
   end
 
   def finish
--- app/models/order.rb
+++ app/models/order.rb
@@ -7,7 +7,6 @@ class Order < ApplicationRecord
 	enum payment_method: {
 		credit_card: 0,
 		bank_transfer: 1
-
 	}
 
 end
