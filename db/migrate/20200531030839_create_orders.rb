cefe6838ae9448b6d781111c3972978df6a46c7d
makiharu <kitakazetoshirokuma@gmail.com>
2020-06-08 06:23:52 +0000

    注文機能の実装

--- db/migrate/20200531030839_create_orders.rb
+++ db/migrate/20200531030839_create_orders.rb
@@ -5,10 +5,10 @@ class CreateOrders < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :postal_code
       t.string :address
-      t.integer :shipping_fees
+      t.integer :shipping_fees, default: 800
       t.integer :payment_method
       t.integer :total_price
-      t.integer :order_status
+      t.integer :order_status, default: 1
 
       t.timestamps
     end
