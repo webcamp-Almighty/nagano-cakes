75190cbd56583991928abab320474a578c0d382f
Takagi Yuki <yuyuyu.eievui@icloud.com>
2020-06-09 04:12:37 +0000

    注文履歴の機能拡張

--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -1,2 +1,16 @@
 class Admins::OrderItemsController < ApplicationController
+
+  def update
+    @order = Order.find(params[:id])
+    if @order_item.update(order_item_params)
+      redirect_to admins_orders_path
+    else
+      render :show
+    end
+  end
+  
+  private
+    def order_item_params
+      params.require(:order_item).permit(:item_status)
+    end
 end
