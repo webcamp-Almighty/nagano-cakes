cbfa052cb05393fb3dbe9471d58f59611bc9944a
makiharu <kitakazetoshirokuma@gmail.com>
2020-06-10 09:57:42 +0000

    バリデーション設定

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -8,6 +8,18 @@ class EndUser < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
 
+  #boolean型のバリデーション
+  validates :is_deleted, inclusion: { in: [true, false] }
+
+  #sign_up,login
+  validates :last_name,presence:true
+  validates :first_name, presence: true
+  validates :last_name_kana,presence:true
+  validates :first_name_kana, presence: true
+  validates :telephone_number, presence:true
+  validates :postal_code, presence:true
+  validates :address, presence:true
+
 
   def active_for_authentication?
     super && (self.is_deleted == false)
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -4,4 +4,7 @@ class OrderItem < ApplicationRecord
 
 	enum item_status:{ creat_not: 0, creat_wait: 1, creat_now: 2, creat_finish: 3}
 	#着手不可,製作待ち,製作中,製作完了
+
+	validates :number, presence: true
+	validates :tax_item_price, presence: true
 end
