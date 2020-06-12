7b201d40d010baee383791895de1ec13f5f16760
makiharu <kitakazetoshirokuma@gmail.com>
2020-06-12 06:51:40 +0000

    商品のテストデータ追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,12 +22,5 @@ class ItemsController < ApplicationController
   	@genres = Genre.all
   end
 
-  # def search
-  #   if params[:name].present?
-  #     @items = Item.where('name LIKE ?', "%#{params[:name]}%")
-  #   else
-  #     @items = Item.none
-  #   end
-  # end
 
 end
