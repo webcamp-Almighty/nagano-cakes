8b0232ed2af446a8b7f35d9a8a396e3ad9cd356d
makiharu <kitakazetoshirokuma@gmail.com>
2020-06-12 07:50:30 +0000

    ページング機能エラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,6 @@ class ItemsController < ApplicationController
   def index
   	@items = Item.all
   	@genres = Genre.all
-    @items = Item.page(params[:page]).reverse_order.per(8)
 
     #条件分岐が必要
     if params[:genre_id]
@@ -11,6 +10,7 @@ class ItemsController < ApplicationController
     	@genre = Genre.find(params[:genre_id])
       #@genre = Genre.find(params[:id])
       @items = @genre.items
+      @items = Item.page(params[:page]).reverse_order.per(8)
     end
 
     @item_ranking = Item.find(Favorite.group(:item_id).order('count(item_id) desc').limit(3).pluck(:item_id))
