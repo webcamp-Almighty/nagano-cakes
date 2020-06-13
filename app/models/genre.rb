339f32c4c4b5acbf06bf897835e175400e9b2ad0
makiharu <kitakazetoshirokuma@gmail.com>
2020-06-13 05:57:12 +0000

    ジャンルの重複登録の禁止

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,9 @@
 class Genre < ApplicationRecord
 	has_many :items
-	validates :name,  presence: true, length: { maximum: 20 }
+	validates :name, presence: true,
+					 uniqueness: true,
+					 length: { maximum: 20 }
+
 	validates :is_invalid, inclusion: { in: [true, false] }
 end
+
