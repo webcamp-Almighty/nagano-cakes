8ad8894f8a25cd4ecbd6eaa4d64e50dcf2d6a5fb
makiharu <kitakazetoshirokuma@gmail.com>
2020-06-13 08:00:18 +0000

    配送先情報入力機能、修正

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -4,4 +4,10 @@ class Delivery < ApplicationRecord
 	#配送先の登録
 	validates :name, presence:true
 	validates :address, presence:true
+
+
+	#複数カラムの結合　登録先住所+(名前)
+	def view_address_and_name
+	   self.address + '(' + self.name + ')'
+	end
 end
